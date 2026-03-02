7d411920c83398010388b46f45b264e971eded69
DariusClover <dario503030@gmail.com>
2026-03-02 15:55:09 -0500

    Primer commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>¡Hola, mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#754a4a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
